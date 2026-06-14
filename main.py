97501c0faeb21d3f1ffb3eb93d0b65634465fa4d
pradeepshetage <pradeepsensible@gmail.com>
2026-06-14 12:42:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Nifty AI Bot Started Successfully")
+import os
+
+print("Nifty AI Bot Started")
+
+api_key = os.getenv("UPSTOX_API_KEY")
+access_token = os.getenv("UPSTOX_ACCESS_TOKEN")
+
+if api_key:
+    print("API Key Loaded Successfully")
+else:
+    print("API Key Missing")
+
+if access_token:
+    print("Access Token Loaded Successfully")
+else:
+    print("Access Token Missing")
