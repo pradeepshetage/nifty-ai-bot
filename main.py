bd2043248e410689a65b691c981fdf7637a3a069
pradeepshetage <pradeepsensible@gmail.com>
2026-06-14 21:27:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+import os
 import upstox_client
 
-print("SDK VERSION TEST")
-print(dir(upstox_client))
+print("UPSTOX PROFILE API TEST")
+
+access_token = os.getenv("UPSTOX_ACCESS_TOKEN")
+
+configuration = upstox_client.Configuration()
+configuration.access_token = access_token
+
+api_client = upstox_client.ApiClient(configuration)
+
+print("SDK Connected Successfully")
+
+print([x for x in dir(upstox_client) if "User" in x])
