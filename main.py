e00075f9b086cce992e07929e94b7ed7a7c95309
pradeepshetage <pradeepsensible@gmail.com>
2026-06-14 21:58:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,3 @@
-import os
 import upstox_client
 
-print("LIVE NIFTY TEST")
-
-access_token = os.getenv("UPSTOX_ACCESS_TOKEN")
-
-configuration = upstox_client.Configuration()
-configuration.access_token = access_token
-
-api_client = upstox_client.ApiClient(configuration)
-
-try:
-    quote_api = upstox_client.MarketQuoteApi(api_client)
-
-    response = quote_api.ltp(
-        "NSE_INDEX|Nifty 50",
-        "2.0"
-    )
-
-    print(response)
-
-except Exception as e:
-    print("ERROR:", str(e))
+print([x for x in dir(upstox_client) if "History" in x or "Candle" in x])
