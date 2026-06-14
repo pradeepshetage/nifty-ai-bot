6ea181e56c1d42c3f0ef48d7c8d74016c7d08216
pradeepshetage <pradeepsensible@gmail.com>
2026-06-14 22:08:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,4 @@
-import os
+import inspect
 import upstox_client
 
-print("NIFTY CANDLE TEST")
-
-access_token = os.getenv("UPSTOX_ACCESS_TOKEN")
-
-configuration = upstox_client.Configuration()
-configuration.access_token = access_token
-
-api_client = upstox_client.ApiClient(configuration)
-
-try:
-    history_api = upstox_client.HistoryApi(api_client)
-
-    print(dir(history_api))
-
-except Exception as e:
-    print("ERROR:", str(e))
+print(inspect.signature(upstox_client.HistoryApi.get_intra_day_candle_data))
