a28a4962baeb42f017057210f2431344c0f925bf
pradeepshetage <pradeepsensible@gmail.com>
2026-06-14 20:29:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import os
 
-print("UPSTOX CONNECTION TEST")
+print("UPSTOX API TEST")
 
 api_key = os.getenv("UPSTOX_API_KEY")
 access_token = os.getenv("UPSTOX_ACCESS_TOKEN")
 
 print("API Key Loaded:", bool(api_key))
 print("Access Token Loaded:", bool(access_token))
+
+print("Ready for Upstox API connection")
