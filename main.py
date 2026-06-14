e8a84d477b8502f4cadb2efc9309ec133a48e0e9
pradeepshetage <pradeepsensible@gmail.com>
2026-06-14 21:52:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import upstox_client
 
-print([x for x in dir(upstox_client) if "Market" in x or "Quote" in x or "History" in x])
+print([x for x in dir(upstox_client) if "Market" in x or "Quote" in x])
