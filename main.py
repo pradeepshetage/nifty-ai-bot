28d7fdf351b33a891d8d41d779f36ddfc2181689
pradeepshetage <pradeepsensible@gmail.com>
2026-06-14 22:04:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import os
 import upstox_client
 
-print("HISTORY API METHODS")
+print("NIFTY CANDLE TEST")
 
-print(dir(upstox_client.HistoryApi))
+access_token = os.getenv("UPSTOX_ACCESS_TOKEN")
+
+configuration = upstox_client.Configuration()
+configuration.access_token = access_token
+
+api_client = upstox_client.ApiClient(configuration)
+
+try:
+    history_api = upstox_client.HistoryApi(api_client)
+
+    print(dir(history_api))
+
+except Exception as e:
+    print("ERROR:", str(e))
