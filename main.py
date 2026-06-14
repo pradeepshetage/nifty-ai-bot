a7c11b94c6635615e4b3bd38044abd64c6b45120
pradeepshetage <pradeepsensible@gmail.com>
2026-06-14 21:49:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,3 @@
-import os
 import upstox_client
 
-print("PROFILE FETCH TEST")
-
-access_token = os.getenv("UPSTOX_ACCESS_TOKEN")
-
-configuration = upstox_client.Configuration()
-configuration.access_token = access_token
-
-api_client = upstox_client.ApiClient(configuration)
-
-try:
-    user_api = upstox_client.UserApi(api_client)
-
-    response = user_api.get_profile("2.0")
-
-    print(response)
-
-except Exception as e:
-    print("ERROR:", str(e))
+print([x for x in dir(upstox_client) if "Market" in x or "Quote" in x or "History" in x])
