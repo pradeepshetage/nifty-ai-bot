4ac9e948d4836ade28834c4f9d4d4bc35e2ae345
pradeepshetage <pradeepsensible@gmail.com>
2026-06-14 21:54:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+import os
 import upstox_client
 
-print([x for x in dir(upstox_client) if "Market" in x or "Quote" in x])
+print("NIFTY QUOTE TEST")
+
+access_token = os.getenv("UPSTOX_ACCESS_TOKEN")
+
+configuration = upstox_client.Configuration()
+configuration.access_token = access_token
+
+api_client = upstox_client.ApiClient(configuration)
+
+try:
+    print([x for x in dir(upstox_client.MarketQuoteApi) if not x.startswith("_")])
+except Exception as e:
+    print("ERROR:", e)
