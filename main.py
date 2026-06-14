9f5ecc90069aaffd278b80f9c1694f62f8ade8aa
pradeepshetage <pradeepsensible@gmail.com>
2026-06-14 21:19:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,2 @@
 import os
-import upstox_client
-
-print("UPSTOX ACCOUNT TEST")
-
-access_token = os.getenv("UPSTOX_ACCESS_TOKEN")
-
-configuration = upstox_client.Configuration()
-configuration.access_token = access_token
-
-api_client = upstox_client.ApiClient(configuration)
-
-print("SDK Connected Successfully")
-
-print("Access Token Length:", len(access_token))
+import upstox
