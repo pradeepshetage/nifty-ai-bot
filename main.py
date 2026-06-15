8e9b31b1324d99fd1d35a8c841fa27076f31c861
pradeepshetage <pradeepsensible@gmail.com>
2026-06-15 20:51:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,23 @@ import upstox_client
 
 ACCESS_TOKEN = os.getenv("UPSTOX_ACCESS_TOKEN")
 
-print("TOKEN FOUND:", ACCESS_TOKEN is not None)
-
 configuration = upstox_client.Configuration()
 configuration.access_token = ACCESS_TOKEN
 
 api_client = upstox_client.ApiClient(configuration)
 
-print(dir(upstox_client))
+history_api = upstox_client.HistoryApi(api_client)
+
+try:
+    response = history_api.get_intra_day_candle_data(
+        "NSE_INDEX|Nifty 50",
+        "5minute",
+        "2.0"
+    )
+
+    print("SUCCESS")
+    print(response)
+
+except Exception as e:
+    print("ERROR")
+    print(str(e))
