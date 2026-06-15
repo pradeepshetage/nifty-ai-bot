e44296e0b12a1e86fda5c596adf0a84e829a5999
pradeepshetage <pradeepsensible@gmail.com>
2026-06-16 00:36:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import upstox_client
+import inspect
 
 print("TOKEN TEST")
 
@@ -12,20 +13,4 @@ api_client = upstox_client.ApiClient(configuration)
 
 history_api = upstox_client.HistoryApi(api_client)
 
-try:
-    print(dir(history_api))
-
-    response = history_api.get_historical_candle_data(
-        "NSE_INDEX|Nifty 50",
-        "day",
-        "2026-06-15",
-        "2026-06-01",
-        "2.0"
-    )
-
-    print("HISTORICAL SUCCESS")
-    print(response)
-
-except Exception as e:
-    print("CANDLE FAILED")
-    print(str(e))
+print(inspect.signature(history_api.get_historical_candle_data))
