a998ed7e23805b42c5b744d997e22d9774b612ef
pradeepshetage <pradeepsensible@gmail.com>
2026-06-15 23:29:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
-import inspect
+import os
 import upstox_client
 
-print("OPTION CHAIN SIGNATURE")
-print(inspect.signature(upstox_client.OptionsApi.get_put_call_option_chain))
+ACCESS_TOKEN = os.getenv("UPSTOX_ACCESS_TOKEN")
 
-print("OPTION CONTRACT SIGNATURE")
-print(inspect.signature(upstox_client.OptionsApi.get_option_contracts))
+configuration = upstox_client.Configuration()
+configuration.access_token = ACCESS_TOKEN
+
+api_client = upstox_client.ApiClient(configuration)
+
+options_api = upstox_client.OptionsApi(api_client)
+
+print("OPTION CONTRACT TEST")
+
+try:
+    response = options_api.get_option_contracts(
+        "NSE_INDEX|Nifty 50"
+    )
+
+    print(response)
+    
+    try:
+        print(response.to_dict())
+    except:
+        pass
+
+except Exception as e:
+    print("ERROR")
+    print(str(e))
