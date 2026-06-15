33137749f1ce034d6351502a57e9fd041f120953
pradeepshetage <pradeepsensible@gmail.com>
2026-06-15 23:01:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 import upstox_client
 
-print(upstox_client.PlaceOrderRequest(
-    quantity=75,
-    product="D",
-    validity="DAY",
-    price=0,
-    instrument_token="TEST",
-    order_type="MARKET",
-    transaction_type="BUY"
-))
+for x in dir(upstox_client):
+    if "Instrument" in x or "Option" in x:
+        print(x)
