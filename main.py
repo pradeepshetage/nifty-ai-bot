7f74aedc7ef760c8221e85820bdde89cc4f475ae
pradeepshetage <pradeepsensible@gmail.com>
2026-06-15 23:35:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 import os
+import upstox_client
 
-print("TOKEN CHECK")
-print(os.getenv("UPSTOX_ACCESS_TOKEN")[:20])
+ACCESS_TOKEN = os.getenv("UPSTOX_ACCESS_TOKEN")
+
+configuration = upstox_client.Configuration()
+configuration.access_token = ACCESS_TOKEN
+
+api_client = upstox_client.ApiClient(configuration)
+
+user_api = upstox_client.UserApi(api_client)
+
+print("PROFILE TEST")
+
+try:
+    response = user_api.get_profile("2.0")
+    print(response)
+except Exception as e:
+    print(e)
