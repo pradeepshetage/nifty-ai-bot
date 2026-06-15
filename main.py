0f891cf93e2586144dbb276b7af5236f46353554
pradeepshetage <pradeepsensible@gmail.com>
2026-06-15 22:35:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
+import pandas as pd
 import upstox_client
 
-print("CANDLE TEST V3")
+print("NIFTY AI BOT")
 
 ACCESS_TOKEN = os.getenv("UPSTOX_ACCESS_TOKEN")
 
@@ -12,21 +13,43 @@ api_client = upstox_client.ApiClient(configuration)
 
 history_api = upstox_client.HistoryApi(api_client)
 
-try:
-    response = history_api.get_intra_day_candle_data(
-        "NSE_INDEX|Nifty 50",
-        "1minute",
-        "2.0"
-    )
+response = history_api.get_intra_day_candle_data(
+    "NSE_INDEX|Nifty 50",
+    "1minute",
+    "2.0"
+)
 
-    print("SUCCESS")
-    print(response)
+candles = response.to_dict()["data"]["candles"]
 
-    try:
-        print(response.to_dict())
-    except Exception as e:
-        print("to_dict failed:", str(e))
+df = pd.DataFrame(
+    candles,
+    columns=[
+        "timestamp",
+        "open",
+        "high",
+        "low",
+        "close",
+        "volume",
+        "oi"
+    ]
+)
 
-except Exception as e:
-    print("ERROR")
-    print(str(e))
+df = df.iloc[::-1]
+
+df["EMA20"] = df["close"].ewm(span=20).mean()
+df["EMA50"] = df["close"].ewm(span=50).mean()
+
+latest = df.iloc[-1]
+
+print("PRICE :", latest["close"])
+print("EMA20 :", round(latest["EMA20"],2))
+print("EMA50 :", round(latest["EMA50"],2))
+
+if latest["EMA20"] > latest["EMA50"]:
+    print("SIGNAL : BUY CE")
+
+elif latest["EMA20"] < latest["EMA50"]:
+    print("SIGNAL : BUY PE")
+
+else:
+    print("SIGNAL : NO TRADE")
