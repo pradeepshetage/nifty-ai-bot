a282c0d412648f77beac3fabc43beefc8d55b302
pradeepshetage <pradeepsensible@gmail.com>
2026-06-16 00:18:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ api_client = upstox_client.ApiClient(configuration)
 history_api = upstox_client.HistoryApi(api_client)
 
 try:
+    print(dir(history_api))
+    
     response = history_api.get_intra_day_candle_data(
         "NSE_INDEX|Nifty 50",
         "1minute",
