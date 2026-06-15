6ed65800d93dba78f0bb74c8b35ed28e65a658d0
pradeepshetage <pradeepsensible@gmail.com>
2026-06-15 22:53:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-import inspect
 import upstox_client
 
-print(inspect.signature(upstox_client.PlaceOrderRequest))
+print(upstox_client.PlaceOrderRequest(
+    quantity=75,
+    product="D",
+    validity="DAY",
+    price=0,
+    instrument_token="TEST",
+    order_type="MARKET",
+    transaction_type="BUY"
+))
