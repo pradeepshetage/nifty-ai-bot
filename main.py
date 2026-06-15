0d76e1fd01ef452783bea0a65cb84ee077813c3e
pradeepshetage <pradeepsensible@gmail.com>
2026-06-15 20:23:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 import inspect
 import upstox_client
 
-print(inspect.signature(upstox_client.HistoryApi.get_intra_day_candle_data))
+from upstox_client.rest import ApiException
+
+try:
+    response = history_api.get_intra_day_candle_data(
+        "NSE_INDEX|Nifty 50",
+        "5minute",
+        "2.0"
+    )
+
+    print("SUCCESS")
+    print(response)
+
+except ApiException as e:
+    print("ERROR")
+    print(e)
+
+except Exception as e:
+    print("ERROR")
+    print(str(e))
