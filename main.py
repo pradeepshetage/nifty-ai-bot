aca75ed44531c139b67e6558e37d40e7eecd0396
pradeepshetage <pradeepsensible@gmail.com>
2026-06-15 23:05:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import upstox_client
 
+print("INSTRUMENT SEARCH")
+
 for x in dir(upstox_client):
-    if "Instrument" in x or "Option" in x:
+    if "Instrument" in x:
+        print(x)
+
+for x in dir(upstox_client):
+    if "Option" in x:
         print(x)
