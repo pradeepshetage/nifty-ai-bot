5f14b592cf6ca9f542bdbaa5fc2a996546e05068
pradeepshetage <pradeepsensible@gmail.com>
2026-06-16 23:01:37 +0530

    Remove unused historical data retrieval code
    Removed historical candle data retrieval and related print statements.

--- crude_bot.py
+++ crude_bot.py
@@ -8,17 +8,4 @@ configuration.access_token = ACCESS_TOKEN
 
 api_client = upstox_client.ApiClient(configuration)
 
-history_api = upstox_client.HistoryApi(api_client)
-
-response = history_api.get_historical_candle_data(
-    "NSE_INDEX|Nifty 50",
-    "day",
-    "2025-06-15",
-    "2.0"
-)
-
-print("STATUS")
-print(response.status)
-
-print("TOTAL")
-print(len(response.data.candles))
+print(dir(upstox_client))
