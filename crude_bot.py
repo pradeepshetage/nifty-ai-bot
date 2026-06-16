f3c1d0658730b590e6bf813defcac89a9f7ebd45
pradeepshetage <pradeepsensible@gmail.com>
2026-06-16 21:40:12 +0530

    Update crude_bot.py

--- crude_bot.py
+++ crude_bot.py
@@ -1,1 +1,11 @@
-print("CRUDE BOT SERVICE WORKING")
+import os
+import upstox_client
+
+print("CRUDE BOT STARTED")
+
+ACCESS_TOKEN = os.getenv("UPSTOX_ACCESS_TOKEN")
+
+if ACCESS_TOKEN:
+    print("TOKEN FOUND")
+else:
+    print("TOKEN MISSING")
