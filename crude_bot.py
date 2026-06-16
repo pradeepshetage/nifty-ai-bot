e4c73d935307ca4f5c84f36e12c39fc1ad5aadcb
pradeepshetage <pradeepsensible@gmail.com>
2026-06-16 19:25:30 +0530

    Create crude_bot.py

---
+++ crude_bot.py
@@ -0,0 +1,15 @@
+import os
+import upstox_client
+
+print("CRUDE BOT STARTED")
+
+ACCESS_TOKEN = os.getenv("UPSTOX_ACCESS_TOKEN")
+
+configuration = upstox_client.Configuration()
+configuration.access_token = ACCESS_TOKEN
+
+api_client = upstox_client.ApiClient(configuration)
+
+history_api = upstox_client.HistoryApi(api_client)
+
+print("UPSTOX CONNECTED")
