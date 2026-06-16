ecc83257005a8d0cfe5d53865f91352c99921d37
pradeepshetage <pradeepsensible@gmail.com>
2026-06-16 07:16:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,36 @@
-candles = response['data']['candles']
+import os
+import upstox_client
 
-print("TOTAL CANDLES:", len(candles))
-print("FIRST CANDLE:", candles[0])
+print("TOKEN TEST")
+
+ACCESS_TOKEN = os.getenv("UPSTOX_ACCESS_TOKEN")
+
+configuration = upstox_client.Configuration()
+configuration.access_token = ACCESS_TOKEN
+
+api_client = upstox_client.ApiClient(configuration)
+
+history_api = upstox_client.HistoryApi(api_client)
+
+try:
+    response = history_api.get_historical_candle_data(
+        "NSE_INDEX|Nifty 50",
+        "day",
+        "2025-06-15",
+        "2.0"
+    )
+
+    candles = response['data']['candles']
+
+    print("TOTAL CANDLES:", len(candles))
+
+    if len(candles) > 0:
+        print("FIRST CANDLE:")
+        print(candles[0])
+
+        print("LAST CANDLE:")
+        print(candles[-1])
+
+except Exception as e:
+    print("FAILED")
+    print(str(e))
