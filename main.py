73d9e78d6291be70ca017fb5c3566f2d5ae8b582
pradeepshetage <pradeepsensible@gmail.com>
2026-06-16 07:06:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,9 @@
-import os
-import upstox_client
-
-ACCESS_TOKEN = os.getenv("UPSTOX_ACCESS_TOKEN")
-
-configuration = upstox_client.Configuration()
-configuration.access_token = ACCESS_TOKEN
-
-api_client = upstox_client.ApiClient(configuration)
-history_api = upstox_client.HistoryApi(api_client)
-
-try:
-    response = history_api.get_historical_candle_data(
-        "NSE_INDEX|Nifty 50",
-        "day",
-        "2025-06-15",
-        "2.0"
-    )
-
-    print(response)
-
-except Exception as e:
-    print(str(e))
+[
+  timestamp,
+  open,
+  high,
+  low,
+  close,
+  volume,
+  oi
+]
