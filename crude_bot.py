5228f85c08d35d40d07c94ba6b0abaeac6055403
pradeepshetage <pradeepsensible@gmail.com>
2026-06-17 21:53:25 +0530

    Update crude_bot.py

--- crude_bot.py
+++ crude_bot.py
@@ -1,11 +1,32 @@
 import os
 import upstox_client
+from pprint import pprint
 
 configuration = upstox_client.Configuration()
 configuration.access_token = os.getenv("UPSTOX_ACCESS_TOKEN")
 
 api_client = upstox_client.ApiClient(configuration)
-
 api = upstox_client.MarketQuoteApi(api_client)
 
-print(api.ltp.__doc__)
+contracts = [
+    "NSE_COM|140106",  # June
+    "NSE_COM|149476"   # July
+]
+
+for symbol in contracts:
+
+    print("\n" + "="*50)
+    print("TESTING:", symbol)
+
+    try:
+
+        response = api.ltp(
+            symbol,
+            "2.0"
+        )
+
+        pprint(response.to_dict())
+
+    except Exception as e:
+
+        print("ERROR:", e)
