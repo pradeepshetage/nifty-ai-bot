746d8248c127166eb4825667b2639c86570d405e
pradeepshetage <pradeepsensible@gmail.com>
2026-06-17 23:24:46 +0530

    Update crude_bot.py

--- crude_bot.py
+++ crude_bot.py
@@ -1,4 +1,35 @@
-import inspect
+import os
 import upstox_client
+from pprint import pprint
 
-print(inspect.signature(upstox_client.PlaceOrderRequest))
+configuration = upstox_client.Configuration()
+configuration.access_token = os.getenv("UPSTOX_ACCESS_TOKEN")
+
+api_client = upstox_client.ApiClient(configuration)
+
+order_api = upstox_client.OrderApi(api_client)
+
+body = upstox_client.PlaceOrderRequest(
+    quantity=10,
+    product="I",
+    validity="DAY",
+    price=0,
+    instrument_token="MCX_FO|520703",
+    order_type="MARKET",
+    transaction_type="BUY",
+    disclosed_quantity=0,
+    trigger_price=0,
+    is_amo=False
+)
+
+try:
+    result = order_api.place_order(
+        body,
+        "2.0"
+    )
+
+    pprint(result)
+
+except Exception as e:
+    print("ERROR:")
+    print(e)
