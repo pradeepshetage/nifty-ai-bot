c420d413a813f0daddfbe81d8fc866ed26d15257
pradeepshetage <pradeepsensible@gmail.com>
2026-06-17 08:26:36 +0530

    Update crude_bot.py

--- crude_bot.py
+++ crude_bot.py
@@ -1,8 +1,9 @@
 import os
 import upstox_client
 import pandas as pd
+from datetime import datetime
 
-print("CRUDE BOT")
+print("CRUDE BOT LIVE")
 
 ACCESS_TOKEN = os.getenv("UPSTOX_ACCESS_TOKEN")
 
@@ -13,32 +14,39 @@ api_client = upstox_client.ApiClient(configuration)
 
 history_api = upstox_client.HistoryApi(api_client)
 
-response = history_api.get_historical_candle_data(
-    "NSE_COM|140106",
-    "day",
-    "2026-06-17",
-    "2.0"
-)
+try:
+    response = history_api.get_intra_day_candle_data(
+        "NSE_COM|140106",
+        "5minute",
+        "2.0"
+    )
 
-candles = response.data.candles
+    candles = response.data.candles
 
-df = pd.DataFrame(
-    candles,
-    columns=["datetime","open","high","low","close","volume","oi"]
-)
+    df = pd.DataFrame(
+        candles,
+        columns=["datetime","open","high","low","close","volume","oi"]
+    )
 
-df = df.sort_values("datetime")
+    df = df.sort_values("datetime")
 
-df["EMA20"] = df["close"].ewm(span=20).mean()
-df["EMA50"] = df["close"].ewm(span=50).mean()
+    df["EMA20"] = df["close"].ewm(span=20).mean()
+    df["EMA50"] = df["close"].ewm(span=50).mean()
 
-ema20 = df["EMA20"].iloc[-1]
-ema50 = df["EMA50"].iloc[-1]
+    price = df["close"].iloc[-1]
+    ema20 = df["EMA20"].iloc[-1]
+    ema50 = df["EMA50"].iloc[-1]
 
-print("EMA20:", round(ema20,2))
-print("EMA50:", round(ema50,2))
+    signal = "BUY" if ema20 > ema50 else "SELL"
 
-if ema20 > ema50:
-    print("BUY SIGNAL")
-else:
-    print("SELL SIGNAL")
+    print("--------------------------------")
+    print("TIME:", datetime.now())
+    print("PRICE:", round(price, 2))
+    print("EMA20:", round(ema20, 2))
+    print("EMA50:", round(ema50, 2))
+    print("SIGNAL:", signal)
+    print("--------------------------------")
+
+except Exception as e:
+    print("ERROR")
+    print(str(e))
