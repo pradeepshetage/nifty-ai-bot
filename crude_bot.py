d2426bc906fc347c014f030ddf3421e930c945d9
pradeepshetage <pradeepsensible@gmail.com>
2026-06-17 06:32:54 +0530

    Update crude_bot.py

--- crude_bot.py
+++ crude_bot.py
@@ -1,1 +1,26 @@
-print("TEST VERSION 999")
+import os
+import upstox_client
+
+print("SEARCH TEST")
+
+ACCESS_TOKEN = os.getenv("UPSTOX_ACCESS_TOKEN")
+
+configuration = upstox_client.Configuration()
+configuration.access_token = ACCESS_TOKEN
+
+api_client = upstox_client.ApiClient(configuration)
+
+instruments_api = upstox_client.InstrumentsApi(api_client)
+
+try:
+    response = instruments_api.search_instrument(
+        query="CRUDEOIL"
+    )
+
+    print("SUCCESS")
+    print(response)
+
+except Exception as e:
+    print("FAILED")
+    print(type(e))
+    print(str(e))
