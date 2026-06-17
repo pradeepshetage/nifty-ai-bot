a387e3a5423433da848f6c7c347ee327a72abe01
pradeepshetage <pradeepsensible@gmail.com>
2026-06-17 22:40:23 +0530

    Implement search for crude oil instrument with error handling
    Added error handling for instrument search.

--- crude_bot.py
+++ crude_bot.py
@@ -1,5 +1,6 @@
 import os
 import upstox_client
+from pprint import pprint
 
 configuration = upstox_client.Configuration()
 configuration.access_token = os.getenv("UPSTOX_ACCESS_TOKEN")
@@ -8,4 +9,15 @@ api_client = upstox_client.ApiClient(configuration)
 
 api = upstox_client.InstrumentsApi(api_client)
 
-print(dir(api))
+try:
+    result = api.search_instrument(
+        "MCX",
+        "CRUDEOILM"
+    )
+
+    pprint(result)
+
+except Exception as e:
+    print("ERROR")
+    print(type(e))
+    print(e)
