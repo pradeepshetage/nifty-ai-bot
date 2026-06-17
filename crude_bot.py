d21348b4df6c7727cfda12ff5bb21e3700464a95
pradeepshetage <pradeepsensible@gmail.com>
2026-06-17 08:03:36 +0530

    Update crude_bot.py

--- crude_bot.py
+++ crude_bot.py
@@ -1,15 +1,1 @@
-import pandas as pd
 
-print("SEARCHING CRUDE")
-
-url = "https://assets.upstox.com/market-quote/instruments/exchange/complete.csv.gz"
-
-df = pd.read_csv(url)
-
-crude = df[
-    df["name"].astype(str).str.contains("CRUDE", case=False, na=False)
-]
-
-print("FOUND:", len(crude))
-
-print(crude[["instrument_key", "tradingsymbol"]].head(10))
