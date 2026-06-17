28f22b6df69e1ee11e03bedb20815b210cade6f1
pradeepshetage <pradeepsensible@gmail.com>
2026-06-17 22:42:39 +0530

    Refactor crude_bot.py to remove search and log signature
    Removed the instrument search functionality and added a print statement for the function signature.

--- crude_bot.py
+++ crude_bot.py
@@ -1,23 +1,11 @@
+import inspect
 import os
 import upstox_client
-from pprint import pprint
 
 configuration = upstox_client.Configuration()
 configuration.access_token = os.getenv("UPSTOX_ACCESS_TOKEN")
 
 api_client = upstox_client.ApiClient(configuration)
-
 api = upstox_client.InstrumentsApi(api_client)
 
-try:
-    result = api.search_instrument(
-        "MCX",
-        "CRUDEOILM"
-    )
-
-    pprint(result)
-
-except Exception as e:
-    print("ERROR")
-    print(type(e))
-    print(e)
+print(inspect.signature(api.search_instrument))
