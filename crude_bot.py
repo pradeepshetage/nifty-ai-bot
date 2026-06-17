d776a77f7039f773e901abfcb32540e8888c9d39
pradeepshetage <pradeepsensible@gmail.com>
2026-06-17 23:07:31 +0530

    Update crude_bot.py

--- crude_bot.py
+++ crude_bot.py
@@ -1,17 +1,2 @@
-import os
 import upstox_client
-from pprint import pprint
-
-configuration = upstox_client.Configuration()
-configuration.access_token = os.getenv("UPSTOX_ACCESS_TOKEN")
-
-api_client = upstox_client.ApiClient(configuration)
-
-api = upstox_client.MarketQuoteApi(api_client)
-
-response = api.ltp(
-    "MCX_FO|520703",
-    "2.0"
-)
-
-pprint(response.to_dict())
+print(dir(upstox_client.OrderApi))
