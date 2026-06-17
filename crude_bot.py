4f61793556058472811595171c699c8704640959
pradeepshetage <pradeepsensible@gmail.com>
2026-06-17 22:45:06 +0530

    Update crude_bot.py

--- crude_bot.py
+++ crude_bot.py
@@ -1,11 +1,14 @@
-import inspect
 import os
 import upstox_client
+from pprint import pprint
 
 configuration = upstox_client.Configuration()
 configuration.access_token = os.getenv("UPSTOX_ACCESS_TOKEN")
 
 api_client = upstox_client.ApiClient(configuration)
+
 api = upstox_client.InstrumentsApi(api_client)
 
-print(inspect.signature(api.search_instrument))
+result = api.search_instrument("CRUDEOILM")
+
+pprint(result)
