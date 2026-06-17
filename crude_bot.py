0c2df1cabae1c3af10d7ee0e73b993fd469fc41e
pradeepshetage <pradeepsensible@gmail.com>
2026-06-17 06:41:08 +0530

    Update crude_bot.py

--- crude_bot.py
+++ crude_bot.py
@@ -1,7 +1,7 @@
 import os
 import upstox_client
 
-print("CRUDE TEST")
+print("BASELINE TEST")
 
 ACCESS_TOKEN = os.getenv("UPSTOX_ACCESS_TOKEN")
 
@@ -12,26 +12,12 @@ api_client = upstox_client.ApiClient(configuration)
 
 history_api = upstox_client.HistoryApi(api_client)
 
-keys = [
-    "MCX_FO|CRUDEOILM",
-    "MCX_COM|CRUDEOILM",
-    "MCX_FO|CRUDEOIL",
-    "MCX_COM|CRUDEOIL"
-]
+response = history_api.get_historical_candle_data(
+    "NSE_INDEX|Nifty 50",
+    "day",
+    "2025-06-15",
+    "2.0"
+)
 
-for key in keys:
-    try:
-        response = history_api.get_historical_candle_data(
-            key,
-            "day",
-            "2025-06-15",
-            "2.0"
-        )
-
-        print("SUCCESS:", key)
-        print("CANDLES:", len(response.data.candles))
-
-    except Exception as e:
-        print("FAILED:", key)
-        print(str(e))
-        print("--------------------------------")
+print("SUCCESS")
+print(len(response.data.candles))
