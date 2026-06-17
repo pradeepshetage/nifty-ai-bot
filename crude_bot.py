d74096ddb7211ad68660bbb9c87a636e5bddfa38
pradeepshetage <pradeepsensible@gmail.com>
2026-06-17 21:44:27 +0530

    Update crude_bot.py

--- crude_bot.py
+++ crude_bot.py
@@ -8,9 +8,25 @@ configuration.access_token = os.getenv("UPSTOX_ACCESS_TOKEN")
 api_client = upstox_client.ApiClient(configuration)
 api = upstox_client.MarketQuoteApi(api_client)
 
-response = api.get_full_market_quote(
-    "NSE_COM|140106",
-    "2.0"
-)
+contracts = {
+    "CRUDE_JUNE": "NSE_COM|140106",
+    "CRUDE_JULY": "NSE_COM|149476",
+}
 
-pprint(response.to_dict())
+for name, instrument in contracts.items():
+
+    print("\n" + "="*50)
+    print(name)
+    print(instrument)
+    print("="*50)
+
+    try:
+        response = api.ltp(
+            instrument_key=instrument,
+            api_version="2.0"
+        )
+
+        pprint(response.to_dict())
+
+    except Exception as e:
+        print("ERROR:", e)
