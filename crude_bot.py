06397da590d590d853cd2698f2a34b8b7d85c387
pradeepshetage <pradeepsensible@gmail.com>
2026-06-17 22:38:25 +0530

    Update crude_bot.py

--- crude_bot.py
+++ crude_bot.py
@@ -6,6 +6,6 @@ configuration.access_token = os.getenv("UPSTOX_ACCESS_TOKEN")
 
 api_client = upstox_client.ApiClient(configuration)
 
-for name in dir(upstox_client):
-    if "Instrument" in name or "instrument" in name:
-        print(name)
+api = upstox_client.InstrumentsApi(api_client)
+
+print(dir(api))
