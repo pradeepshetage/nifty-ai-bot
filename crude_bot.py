93160a25c58855ae855ce1ae22c8a01b168c3394
pradeepshetage <pradeepsensible@gmail.com>
2026-06-17 20:05:36 +0530

    Update crude_bot.py

--- crude_bot.py
+++ crude_bot.py
@@ -1,11 +1,25 @@
 import os
 import upstox_client
 
+print("LTP TEST")
+
 configuration = upstox_client.Configuration()
 configuration.access_token = os.getenv("UPSTOX_ACCESS_TOKEN")
 
 api_client = upstox_client.ApiClient(configuration)
 
-api = upstox_client.UserApi(api_client)
+api = upstox_client.MarketQuoteApi(api_client)
 
-print(api.get_profile("2.0"))
+try:
+    response = api.ltp(
+        "NSE_COM|140106",
+        "2.0"
+    )
+
+    print("SUCCESS")
+    print(response)
+
+except Exception as e:
+    print("ERROR")
+    print(type(e))
+    print(str(e))
