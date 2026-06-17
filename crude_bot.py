b879868e25eee745a7525ac947c68be9d7795f89
pradeepshetage <pradeepsensible@gmail.com>
2026-06-17 22:49:31 +0530

    Use pprint to display instrument data

--- crude_bot.py
+++ crude_bot.py
@@ -1,5 +1,6 @@
 import os
 import upstox_client
+from pprint import pprint
 
 configuration = upstox_client.Configuration()
 configuration.access_token = os.getenv("UPSTOX_ACCESS_TOKEN")
@@ -12,6 +13,4 @@ result = api.search_instrument("CRUDE")
 
 for x in result.data:
     print("================================")
-    print("NAME:", x.name)
-    print("KEY:", x.instrument_key)
-    print("EXPIRY:", x.last_trading_date)
+    pprint(x)
