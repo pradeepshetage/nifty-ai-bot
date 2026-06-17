cd17df50a95cc50712e70935a75a3c7dcf642bed
pradeepshetage <pradeepsensible@gmail.com>
2026-06-17 18:49:56 +0530

    Update crude_bot.py

--- crude_bot.py
+++ crude_bot.py
@@ -1,13 +1,8 @@
 import os
-import upstox_client
 
-ACCESS_TOKEN = os.getenv("UPSTOX_ACCESS_TOKEN")
+token = os.getenv("UPSTOX_ACCESS_TOKEN")
 
-configuration = upstox_client.Configuration()
-configuration.access_token = ACCESS_TOKEN
+print("TOKEN EXISTS:", token is not None)
 
-api_client = upstox_client.ApiClient(configuration)
-
-api = upstox_client.UserApi(api_client)
-
-print(api.get_profile("2.0"))
+if token:
+    print("TOKEN LENGTH:", len(token))
