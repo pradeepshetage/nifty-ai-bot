7d6cdc127ad411f929dcf3da10b8db2c94ac59c8
pradeepshetage <pradeepsensible@gmail.com>
2026-06-17 08:19:00 +0530

    Update crude_bot.py

--- crude_bot.py
+++ crude_bot.py
@@ -1,22 +1,33 @@
+import os
+import upstox_client
 import pandas as pd
 
-print("ACTIVE CRUDE CONTRACTS")
+print("JUNE CRUDE TEST")
 
-url = "https://assets.upstox.com/market-quote/instruments/exchange/complete.csv.gz"
+ACCESS_TOKEN = os.getenv("UPSTOX_ACCESS_TOKEN")
 
-df = pd.read_csv(url)
+configuration = upstox_client.Configuration()
+configuration.access_token = ACCESS_TOKEN
 
-crude = df[
-    (df["name"].astype(str).str.contains("CRUDEOILM", case=False, na=False))
-]
+api_client = upstox_client.ApiClient(configuration)
 
-print(
-    crude[
-        [
-            "instrument_key",
-            "tradingsymbol",
-            "expiry",
-            "instrument_type"
-        ]
-    ].to_string()
+history_api = upstox_client.HistoryApi(api_client)
+
+response = history_api.get_historical_candle_data(
+    "NSE_COM|140106",
+    "day",
+    "2026-06-17",
+    "2.0"
 )
+
+candles = response.data.candles
+
+df = pd.DataFrame(
+    candles,
+    columns=["datetime","open","high","low","close","volume","oi"]
+)
+
+print("TOTAL CANDLES:", len(df))
+
+print(df[["datetime","open","high","low","close"]].head())
+print(df[["datetime","open","high","low","close"]].tail())
