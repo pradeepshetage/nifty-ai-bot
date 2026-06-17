ce550c4a254546d33164e27d17b0fbd4ca582e07
pradeepshetage <pradeepsensible@gmail.com>
2026-06-17 18:40:52 +0530

    Update crude_bot.py

--- crude_bot.py
+++ crude_bot.py
@@ -1,8 +1,6 @@
 import os
 import upstox_client
 
-print("TOKEN TEST")
-
 ACCESS_TOKEN = os.getenv("UPSTOX_ACCESS_TOKEN")
 
 configuration = upstox_client.Configuration()
@@ -12,13 +10,4 @@ api_client = upstox_client.ApiClient(configuration)
 
 api = upstox_client.UserApi(api_client)
 
-try:
-    response = api.get_profile("2.0")
-
-    print("SUCCESS")
-    print(response)
-
-except Exception as e:
-    print("FAILED")
-    print(type(e))
-    print(str(e))
+print(api.get_profile("2.0"))
